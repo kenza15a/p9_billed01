0a69694037a369956ff2ace5fe9948f225245438
kenza filali <filali1991.univ@gmail.com>
2022-12-18 10:39:30 +0100

    test form in newbill js
    reste un prbleme avec bills.js iconeeye

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -9,8 +9,8 @@ import mockStore from "../__mocks__/store"
 import router from "../app/Router.js";
 import { ROUTES, ROUTES_PATH } from "../constants/routes";
 import NewBillUI from "../views/NewBillUI.js";
+import NewBill from "../containers/NewBill.js"
 import { localStorageMock } from "../__mocks__/localStorage";
-
 jest.mock("../app/store", () => mockStore)
 describe("Given I am connected as an employee", () => {
 
@@ -27,9 +27,23 @@ describe("Given I am connected as an employee", () => {
       const mailIcon = screen.getByTestId('icon-mail')
       expect(mailIcon.classList.contains("active-icon")).toBeTruthy();
     })
+
+    test("the the text Envoyer une note de frais  should be displayed on top of the page", () => {
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({ type: 'Employee' }))
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+      window.onNavigate(ROUTES_PATH.NewBill)
+      //await waitFor(() => screen.getAllByText('Envoyer une note de frais'))
+      const title = screen.getAllByText('Envoyer une note de frais')
+      expect(title).toBeTruthy();
+    })
+
   })
 
-  test("when i click on the button nouvelle note de frais the modal appearse with th fields", () => {
+  test("when i click on the button nouvelle note de frais the form appearse with it's fields", () => {
     document.body.innerHTML = NewBillUI()
     expect(screen.getByTestId("form-new-bill")).toBeTruthy()// le formilaire
     expect(screen.getByTestId("expense-type")).toBeTruthy()
@@ -42,7 +56,84 @@ describe("Given I am connected as an employee", () => {
     expect(screen.getByTestId("file")).toBeTruthy()
     expect(screen.getByRole("button")).toBeTruthy()
   })
-  /* test('when i click on the eye icon a modal should show up',()=>{
-     expect(screen.getByTestId("modaleFileEmployee"))
-   })*/
-})
+
+  test('Then I can select upload an image file', () => {
+    Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+    window.localStorage.setItem('user', JSON.stringify({ type: 'Employee', email: "a@a.com" }))
+    document.body.innerHTML = NewBillUI()
+    mockStore.bills = jest.fn().mockImplementation(() => { return { create: () => { Promise.resolve({}) } } })
+    const onNavigate = (pathname) => { document.body.innerHTML = pathname }
+    const newBill = new NewBill({ document, onNavigate, store: mockStore, localStorage: window.localStorage })
+    const handleChangeFile = jest.fn(newBill.handleChangeFile)
+    const inputFile = screen.getByTestId("file")
+    expect(inputFile).toBeTruthy()
+    const file = new File(["file"], "file.jpg", { type: "image/jpeg" })
+    inputFile.addEventListener("change", handleChangeFile)
+    fireEvent.change(inputFile, { target: { files: [file] } })
+    expect(handleChangeFile).toHaveBeenCalled()
+    expect(inputFile.files).toHaveLength(1)
+    expect(inputFile.files[0].name).toBe("file.jpg")
+    jest.spyOn(window, "alert").mockImplementation(() => { })
+    expect(window.alert).not.toHaveBeenCalled()
+  })
+  //testant si une autre extesnssion pourra pas passer
+  test("Then i check if non image files can't be uploader", () => {
+    document.body.innerHTML = NewBillUI()
+    const store = null
+    const onNavigate = (pathname) => { document.body.innerHTML = pathname }
+    const newBill = new NewBill({ document, onNavigate, store, localStorage: window.localStorage })
+    const handleChangeFile = jest.fn(newBill.handleChangeFile)
+    const inputFile = screen.getByTestId("file")
+    expect(inputFile).toBeTruthy()
+    inputFile.addEventListener("change", handleChangeFile)
+    fireEvent.change(inputFile, { target: { files: [new File(["file.zip"], "file.zip", { type: "file/zip" })] } })
+    expect(handleChangeFile).toHaveBeenCalled()
+    expect(inputFile.files[0].name).not.toBe("file.jpg")
+    jest.spyOn(window, "alert").mockImplementation(() => { })
+    expect(window.alert).toHaveBeenCalled()
+  })
+
+  //tester la validité des champs
+
+  describe('Given I am a user connected as Employee', () => {
+    describe("When I submit the form completed", () => {
+      test("Then the bill is created", () => {
+        document.body.innerHTML = NewBillUI()
+        const onNavigate = (pathname) => { document.body.innerHTML = ROUTES({ pathname }) }
+        Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+        window.localStorage.setItem('user', JSON.stringify({ type: 'Employee', email: "a@a.com" }))
+        const newBill = new NewBill({ document, onNavigate, store: null, localStorage: window.localStorage })
+        const billMock = {
+          type: "Restaurants et bars",
+          name: "Vol Paris Amesterdam",
+          date: "2022-05-11",
+          amount: 120,
+          vat: 70,
+          pct: 30,
+          commentary: " restau italien pour une reunion ",
+          fileUrl: "../img/0.jpg",
+          fileName: "darkhorse.jpg",
+          status: "pending"
+        }
+        screen.getByTestId("expense-type").value = billMock.type
+        screen.getByTestId("expense-name").value = billMock.name
+        screen.getByTestId("datepicker").value = billMock.date
+        screen.getByTestId("amount").value = billMock.amount
+        screen.getByTestId("vat").value = billMock.vat
+        screen.getByTestId("pct").value = billMock.pct
+        screen.getByTestId("commentary").value = billMock.commentary
+        newBill.fileName = billMock.fileName
+        newBill.fileUrl = billMock.fileUrl
+        newBill.updateBill = jest.fn()
+        const handleSubmit = jest.fn((e) => newBill.handleSubmit(e))
+        const form = screen.getByTestId("form-new-bill")
+        form.addEventListener("submit", handleSubmit)
+        fireEvent.submit(form)
+        expect(handleSubmit).toHaveBeenCalled()
+        expect(newBill.updateBill).toHaveBeenCalled()
+      })
+
+    })
+
+
+  })})
--- Billed-app-FR-Front/src/containers/NewBill.js
+++ Billed-app-FR-Front/src/containers/NewBill.js
@@ -67,7 +67,7 @@ export default class NewBill {
     console.log(extenssions);
     const filePath = e.target.value.split(/\\/g)
     const fileName = filePath[filePath.length - 1]
-    console.log(fileName);
+    console.log(fileName);//pour separer le nom du fichier de l'extenssion
     const fileExtention = fileName.split('.')[1]; // recuperation de l'extenssion du fichier
     console.log(fileExtention);
     const formData = new FormData()
@@ -96,7 +96,26 @@ export default class NewBill {
         }).catch(error => console.error(error))
     }
   }
-
+  handleSubmit = e => {
+    e.preventDefault()
+    console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector(`input[data-testid="datepicker"]`).value)
+    const email = JSON.parse(localStorage.getItem("user")).email
+    const bill = {
+      email,
+      type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
+      name: e.target.querySelector(`input[data-testid="expense-name"]`).value,
+      amount: parseInt(e.target.querySelector(`input[data-testid="amount"]`).value),
+      date: e.target.querySelector(`input[data-testid="datepicker"]`).value,
+      vat: e.target.querySelector(`input[data-testid="vat"]`).value,
+      pct: parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) || 20,
+      commentary: e.target.querySelector(`textarea[data-testid="commentary"]`).value,
+      fileUrl: this.fileUrl,
+      fileName: this.fileName,
+      status: 'pending'
+    }
+    this.updateBill(bill)
+    this.onNavigate(ROUTES_PATH['Bills'])
+  }
 
   // not need to cover this function by tests
   updateBill = (bill) => {
